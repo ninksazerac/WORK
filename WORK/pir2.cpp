6ae4cc417db8400db4540929ca0501695bf6e331
PHANCHITA TERAPHATANAROJ <63010665@KMITL.AC.TH>
2020-08-23 15:39:44 +0700

    finish1

---
+++ WORK/pir2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float r, pi;
+	pi = 3.14;
+	scanf("%f", &r);
+	r = r * r;
+	printf("%f", pi * r);
+	return 0;
+}
---
+++ WORK/result.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float a, b, c;
+	scanf("%f", &a);
+	scanf("%f", &b);
+	scanf("%f", &c);
+	printf("Positive result : %f\n", a + b + c);
+	printf("Multiple result : %f", a * b * c);
+	return 0;
+}

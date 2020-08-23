359b6b0679c6b4f652e784b8dc31e0fe338cde55
PHANCHITA TERAPHATANAROJ <63010665@KMITL.AC.TH>
2020-08-23 15:36:30 +0700

    Add project files.

---
+++ WORK/work1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	char name, position, address;
+	int salary, bonus, ot;
+	printf("Employee name : ");
+	scanf("%s", &name);
+	printf("Employee position : ");
+	scanf("%s", &position);
+	printf("Address : ");
+	scanf("%s", &address);
+	printf("Salary : ");
+	scanf("%s", &salary);
+	printf("Bonus money : ");
+	scanf("%s", &bonus);
+	printf("OT money : ");
+	scanf("%s", &ot);
+	return 0;
+}
